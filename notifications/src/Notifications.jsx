08c99903bf43f4d0c6fc8a1e40f2993480cabe3e
Ryan Lima <ryanlima08021997@gmail.com>
2025-08-02 16:21:36 -0300

    feat: adiciona módulo remoto - notifications

---
+++ notifications/src/Notifications.jsx
@@ -0,0 +1,3 @@
+export default function Notifications() {
+  return <div>🔔 Você tem 3 novas notificações!</div>;
+}
---
+++ notifications/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: 'notifications',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Notifications': './src/Notifications.jsx',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
